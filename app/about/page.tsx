36926a1ddf49839e0f65dbf048a60355199f4074
Tim <timmaina64@gmail.com>
2025-03-23 12:58:27 +0300

    Minor change

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next"
 import AboutPage from "@/components/about/about-page"
 
 export const metadata: Metadata = {
-  title: "About Us | AfyaGo,
+  title: "About Us | AfyaGo",
   description: "Learn about AfyaGo your trusted online pharmacy in Kenya",
 }
 
